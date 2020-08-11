a1a8fde9294def024ab1516df58b177c38c7b551
Gustavo Vieira <guustavov@gmail.com>
2020-08-11 03:35:35 -0300

    Change test.exs to config test scenario with env variables

--- backend/config/test.exs
+++ backend/config/test.exs
@@ -6,10 +6,10 @@ use Mix.Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :mr_heckles, MrHeckles.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "mr_heckles_test#{System.get_env("MIX_TEST_PARTITION")}",
-  hostname: "localhost",
+  username: System.get_env("POSTGRES_TEST_USER") || "postgres",
+  password: System.get_env("POSTGRES_TEST_PASSWORD") || "postgres",
+  database: System.get_env("POSTGRES_TEST_DB") || "mr_heckles_test#{System.get_env("MIX_TEST_PARTITION")}",
+  hostname: System.get_env("POSTGRES_TEST_HOST") || "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
