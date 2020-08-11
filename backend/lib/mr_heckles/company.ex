823980973ede6dc29f4f8a05d78b1d2ff22f1157
Gustavo Vieira <guustavov@gmail.com>
2020-08-11 17:14:52 -0300

    Create companies schema and first migration

---
+++ backend/lib/mr_heckles/company.ex
@@ -0,0 +1,18 @@
+defmodule MrHeckles.Company do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "companies" do
+    field :description, :string
+    field :name, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(company, attrs) do
+    company
+    |> cast(attrs, [:name, :description])
+    |> validate_required([:name])
+  end
+end
---
+++ backend/priv/repo/migrations/20200811200856_create_companies.exs
@@ -0,0 +1,13 @@
+defmodule MrHeckles.Repo.Migrations.CreateCompanies do
+  use Ecto.Migration
+
+  def change do
+    create table(:companies) do
+      add :name, :string, null: false
+      add :description, :string
+
+      timestamps()
+    end
+
+  end
+end
