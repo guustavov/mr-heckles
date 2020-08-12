e69127e76d234732d8bcc931186acc7d2c9b35bc
Gustavo Vieira <guustavov@gmail.com>
2020-08-12 02:13:29 -0300

    Add exmachina dependency and factory

---
+++ backend/test/support/factory.ex
@@ -0,0 +1,15 @@
+defmodule MrHeckles.Factory do
+  @moduledoc """
+  Factories to be used in the tests.
+  """
+  use ExMachina.Ecto, repo: MrHeckles.Repo
+
+  alias MrHeckles.Company
+
+  def company_factory do
+    %Company{
+      name: sequence(:name, &"Company #{&1}"),
+      description: sequence(:description, &"Description of company #{&1}")
+    }
+  end
+end
